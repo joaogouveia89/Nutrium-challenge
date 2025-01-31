8e27951da2c96b2ddee995d3d5108866b0a666d9
Joao Gouveia <jvgouve@sibs.com>
2025-01-31 07:23:38 +0000

    removed redundant label on manifest and clean unused imports on view model

--- app/src/main/java/io/github/joaogouveia89/nutriumchallengejoaogouveia/professionalsList/presenter/viewModel/ProfessionalListViewModel.kt
+++ app/src/main/java/io/github/joaogouveia89/nutriumchallengejoaogouveia/professionalsList/presenter/viewModel/ProfessionalListViewModel.kt
@@ -1,6 +1,5 @@
 package io.github.joaogouveia89.nutriumchallengejoaogouveia.professionalsList.presenter.viewModel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
